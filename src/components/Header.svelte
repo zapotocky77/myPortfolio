6946331f3a406f8583ed2af8025f0114f17d8948
Samuel Zápotocký <samuel.zapotocky@student.tuke.sk>
2024-06-23 22:57:18 +0200

    first commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let y;
+
+    export let tabs = [
+        { name: "Projects", link: "#projects" },
+        { name: "About me", link: "#about" },
+    ];
+</script>
+
+<header class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between " + (
+    y > 0 ? "py-4 bg-slate-950" : "py-6 bg-transparent"
+)}>
+    <h1 class="font-medium">
+        <b class="font-bold poppins">Samuel</b> Zápotocký
+    </h1>
+    <div class="sm:flex ml-auto pr-4 items-center gap-4 hidden">
+        {#each tabs as tab, index}
+            <a href={tab.link} class="duration-200 hover:text-violet-400">
+                <p>{tab.name}</p>
+            </a>
+        {/each}
+        <a
+            href="https://www.instagram.com/samo_zapo/"
+            target="_blank"
+            class="relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
+        >
+            <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
+            </div>
+            <h4 class="relative z-9">Get in touch</h4>
+        </a>
+    </div>
+</header>
---
+++ src/components/Main.svelte
@@ -0,0 +1,175 @@
+<script>
+    import Step from "../components/Step.svelte";
+
+    let steps = [
+        { name: "Java Game", icon: "fa-solid fa-cart-shopping", href: "https://github.com/Samuel2048/SimulatorVraydeniaAlbinov" },
+        { name: "Yet Another Solution", icon: "fa-solid fa-list-check", href: "" },
+        { name: "Hack Košice Team member", icon: "fa-solid fa-diagram-project", href: "https://hackkosice.com/"},
+    ];
+
+    let benefits = [
+        { 
+            name: "a self tought developer", 
+            description: "I taught myself to code using free online resources and absolutely fell in love with the creativity and problem solving that is involved in developing and engineering innovative new online experiences. Starting of with JavaScript, HTML & CSS and evolving my knowledge base to include JavaScript frameworks, backend programming, design, cloud services and much much more."
+        },
+        {
+            name: "a product designer & UX finatic",
+            description: "Carefully crafting and designing amazing user experiences allows me to express and experiment with every morsel of creativity I have. I love the challenge of learning new design concepts and enabling users with amazing online experiences."
+        },
+        {
+            name: "an excellent communicator",
+            description: "Communication is key and it's a paramount value of mine. I believe in transparency and constructive communication above all else. This helps me develop deep relationships and ensures my effectiveness and productivity in any work space with any team."
+        },
+    ];
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section id="introPage" class="grid grid-cols-1 lg:gap-10 py-8 sm:py-14">
+        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+            <h2 class="font-semibold text-2xl sm:text-5xl md:text-6xl">
+                Hi! I'm <span class="poppins text-violet-400">Samuel</span> Zápotocký <br/> Front End
+                <span class="poppins text-violet-400">Developer</span>
+            </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                My <span class="poppins text-violet-400">favorite tech</span> includes HTML, JavaScript (React, SvelteKit), styled-components, TailWindCSS,  Bootstrap 5, Node.js & MySQL or Firebase/Firestore!
+            </p>
+            <a
+                class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative 
+                overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
+                href="https://www.instagram.com/samo_zapo/"
+                target="_blank"
+            >
+                <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200" />
+                <h4 class="relative z-9">Get in touch &rarr;</h4>
+            </a>
+        </div>
+        <div class="relative shadow-2xl grid place-items-center">
+            <img src={"images/profile.png"} alt="Profile image" class="object-cover z-[2] max-h-[70vh]"/>
+        </div>
+    </section>
+
+    <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+        <div class="flex flex-col gap-2 text-center">
+            <h6 class="text-lg sm:text-xl md:text-2xl">
+                A few of my creative endavors.
+            </h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                Curious to <span class="poppins text-violet-400">see</span> my work?
+            </h3>
+        </div>
+        <a
+            href="https://github.com/zapotocky77"
+            target="_blank"
+            class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex 
+            items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200">
+            <i class="fa-brands fa-github"></i>
+            <p>Check out my GitHub!</p>
+        </a>
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+            <Step step={steps[0]}>
+                <p>
+                    Smoljames Store is a a merchanising store created with 
+                    <strong class="text-violet-400">Next.js + Express.js, Commerce.js & Stripe!</strong> Commerce.js is a CMS for managing project and Stripe is used for all transaction handling.
+                </p>
+            </Step>
+            <Step step={steps[1]}>
+                <p>
+                    Test for model robustness with customized test profiles and receive exhaustive reporting on the performance and potential vulnerabilities of your model.
+                </p>
+            </Step>
+            <Step step={steps[2]}>
+                <p>
+                    Augment your datasets with our filters and dataset manipulations to ensure your models are trained on the highest quality datasets (coming soon).
+                </p>
+            </Step>
+        </div>
+    </section>
+    <section id="about" class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative">
+        <div
+            class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 
+            before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+        >
+            <h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                A bit <span class="poppins text-violet-400">about</span> me.
+            </h3>
+        </div>
+        <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">I am ...</p>
+        <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+            {#each benefits as benefit, index}
+                <div class="flex gap-6 sm:gap-8">
+                    <p class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold">
+                        0{index + 1}
+                    </p>
+                    <div class="flex flex-col gap-6 sm:gap-8">
+                        <h3 class="text-2xl sm:text-3xl md:text-5xl">
+                            {benefit.name}
+                        </h3>
+                        <p>{benefit.description}</p>
+                    </div>
+                </div>
+            {/each}
+        </div>
+        <h5 class="text-2xl sm:text-3xl font-semibold text-center poppins">
+            The <span class="text-violet-400 poppins">Complete</span> Package
+        </h5>
+        <div class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full">
+            <table class="bg-white text-slate-700 rounded-lg text-center">
+                <thead class="border-b border-solid border-slate-200">
+                    <tr class="">
+                        <th />
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
+                        <th class="whitespace-nowrap bg-violet-700 text-white rounded-tr-lg p-4 px-8">Me</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap">
+                            Dedication
+                        </td>
+                        <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-check text-green-500"></i></td>
+                    </tr>
+
+                    <tr class="border-b border-solid border-slate-200">
+                        <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap">
+                            Critical Thought
+                        </td>
+                        <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-check text-green-500"></i></td>
+                    </tr>
+
+                    <tr class="border-b border-solid border-slate-200">
+                        <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap">
+                            Interpersonal Skills
+                        </td>
+                        <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-check text-green-500"></i></td>
+                    </tr>
+
+                    <tr class="border-b border-solid border-slate-200 border-b-0">
+                        <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap">
+                            Programming Ability
+                        </td>
+                        <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-check text-green-500"></i></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="mx-auto -mt-12 italic sm:hidden opacity-50">
+            <p>Scroll to see more &rarr;</p>
+        </div>
+        <p class="mx-auto">So why not invest?</p>
+    </section>
+</main>
